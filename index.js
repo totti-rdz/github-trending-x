c94f7a93fa8d9d61057c98f0fd713a475aa8e593
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-04-30 16:04:29 +0200

    scrape trending repository names

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world")
+import axios from "axios";
+import { load } from "cheerio";
+
+const url = "https://github.com/trending/javascript?since=daily";
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = load(html);
+    const repos = [];
+    $("h2.h3 a").each((_, elem) =>
+      repos.push($(elem).text().replace(/\s+/g, ""))
+    );
+
+    console.log("repos", repos);
+  })
+  .catch(console.error);
