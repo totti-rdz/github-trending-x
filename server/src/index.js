636cd03af2cf303bdfd5752a753a11e156db50d9
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-06-04 14:39:38 +0200

    serve client from express backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import Scraper from "./services/scraper.js";
+import path from "path";
 
 const port = 3000;
-
 const app = express();
 
-app.get("/", async (_, res) => {
+const __dirname = path.resolve();
+const pathClient = path.join(__dirname, "..", "client", "dist");
+const pathClientIndex = path.join(pathClient, "index.html");
+
+app.use(express.static(pathClient));
+
+app.get("/repos", async (_, res) => {
   const scraper = new Scraper();
   const result = await scraper.getRepos();
   res.send(result);
+});
+
+app.get("*", (_, res) => {
+  res.sendFile(pathClientIndex);
 });
 
 app.listen(port, () => {
