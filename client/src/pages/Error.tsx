ca6a0d966cd2e4873a6ad79f7e67dd3db19700dd
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-06-04 15:12:36 +0200

    create Error page

---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,21 @@
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      {isRouteErrorResponse(error) ? (
+        <>
+          <h2>{error.status}</h2>
+          <p>{error.statusText}</p>
+          {error.data?.message && <p>{error.data.message}</p>}
+        </>
+      ) : (
+        <p>Sorry, an unexpected error occured.</p>
+      )}
+    </div>
+  );
+}
