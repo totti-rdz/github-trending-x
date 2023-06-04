226acce8cf78107ff971c7aab20040529ad7c59e
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-06-05 00:20:53 +0200

    put client build output directly to server folder

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../server/dist/client",
+  },
   server: {
     proxy: {
       "/api": "http://localhost:3000",
