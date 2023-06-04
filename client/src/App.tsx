4c391282c15c61bbee8e5e77c962b585976a476f
Thorsten Herfurtner <thorsten.herfurtner@empion.io>
2023-06-04 15:28:52 +0200

    implement error page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home.tsx";
 import Other from "./pages/Other.tsx";
+import ErrorPage from "./pages/Error.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/other",
