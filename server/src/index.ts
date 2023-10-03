85b1ead8f47b894c377797286ae520a1c3be7c67
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-03 19:13:14 +0200

    implement redirecting frontend to localhost:5173

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import { routes } from "./routes";
+import { appendQueryParams } from "./utils/appendQueryParams";
 
 dotenv.config();
 
@@ -11,11 +12,20 @@ const app = express();
 const pathClient = path.join(__dirname, "..", "dist", "client");
 const pathClientIndex = path.join(pathClient, "index.html");
 
-app.use(express.static(pathClient));
-
 // Set endpoints for express router routes
 app.use("/api", routes());
 
+app.get("*", (req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    res.redirect(
+      "http://localhost:5173" + req.path + appendQueryParams(req.query)
+    );
+    return;
+  }
+  next();
+});
+
+app.use(express.static(pathClient));
 app.get("*", (_, res) => {
   res.sendFile(pathClientIndex);
 });
