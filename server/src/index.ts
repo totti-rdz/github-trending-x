7374d1b072ac5272b51d50e39524125274897c34
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-03 17:06:41 +0200

    add .env file to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
+import dotenv from "dotenv";
 import { routes } from "./routes";
+
+dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
