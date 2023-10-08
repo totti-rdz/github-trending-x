cd5254a2f950e774079c150431cc8253f2cfe219
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-08 08:44:09 +0200

    fix classNames returning too much spaces for empty strings

--- client/src/components/utils/StringHelper.ts
+++ client/src/components/utils/StringHelper.ts
@@ -1,4 +1,7 @@
 export const StringHelper = {
   capitalize: (string: string) =>
     string.charAt(0).toUpperCase() + string.slice(1),
+
+  isValidString: (string: unknown) =>
+    typeof string === 'string' && string.trim() !== '',
 };
