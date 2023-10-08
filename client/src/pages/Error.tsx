95c13d57734279b3e049f7fe1991e4c36073f3b7
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-08 07:47:16 +0200

    wrap Error page with Layout

--- client/src/pages/Error.tsx
+++ client/src/pages/Error.tsx
@@ -1,21 +1,24 @@
 import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+import Layout from '../components/Layout';
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      {isRouteErrorResponse(error) ? (
-        <>
-          <h2>{error.status}</h2>
-          <p>{error.statusText}</p>
-          {error.data?.message && <p>{error.data.message}</p>}
-        </>
-      ) : (
-        <p>Sorry, an unexpected error occured.</p>
-      )}
-    </div>
+    <Layout>
+      <div id="error-page">
+        <h1>Oops!</h1>
+        {isRouteErrorResponse(error) ? (
+          <>
+            <h2>{error.status}</h2>
+            <p>{error.statusText}</p>
+            {error.data?.message && <p>{error.data.message}</p>}
+          </>
+        ) : (
+          <p>Sorry, an unexpected error occured.</p>
+        )}
+      </div>
+    </Layout>
   );
 }
