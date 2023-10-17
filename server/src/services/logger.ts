606903f36aaa020605c21bc0867f814e973bef3c
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-17 03:52:17 +0200

    add warn method to logger

--- server/src/services/logger.ts
+++ server/src/services/logger.ts
@@ -5,6 +5,12 @@ export const logger = {
       '[INFO] ' + message,
       '\x1b[0m' // reset font color
     ),
+  warn: (message: string) =>
+    console.error(
+      '\x1b[33m%s\x1b[0m', // change font color to yellow
+      '[WARNING] ' + message,
+      '\x1b[0m' // reset font color
+    ),
   error: (message: string) =>
     console.error(
       '\x1b[31m%s\x1b[0m', // change font color to red
