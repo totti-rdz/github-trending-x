260264b09c0f49c753247dcbebb87b48f1357fa3
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-23 06:59:42 +0200

    extract fetching into ApiService class

---
+++ client/src/services/ApiService.ts
@@ -0,0 +1,36 @@
+export class ApiService {
+  public abortController: AbortController;
+  private abortSignal: AbortSignal;
+  public abort: typeof this.abortController.abort;
+
+  constructor() {
+    this.abortController = new AbortController();
+    this.abortSignal = this.abortController.signal;
+    this.abort = () => this.abortController.abort(); // this context will be lost if abort is passed as function and not invoked inside arrow function
+  }
+
+  get isAborted() {
+    return this.abortSignal.aborted;
+  }
+
+  public async fetch<T>(url: string) {
+    try {
+      const response = await fetch(url, { signal: this.abortSignal });
+      if (!response.ok) {
+        throw new Error(
+          `Failed fetching ${url} - Response.ok was falsy. Status: ${response.status} - ${response.statusText}`
+        );
+      }
+      const data: T[] = await response.json();
+
+      return data;
+    } catch (error) {
+      if (this.abortSignal.aborted) {
+        console.info('The user aborted the request');
+      } else {
+        console.error('The request failed with error:', error);
+      }
+      return null;
+    }
+  }
+}
