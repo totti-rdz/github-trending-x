c273186d92a4d75f8629c1762bac9b52352ad9e9
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-24 07:55:36 +0200

    extract RepositoryCard

---
+++ client/src/components/RepositoryCard/index.tsx
@@ -0,0 +1,45 @@
+import { Repo } from '../../hooks/useRepositories';
+
+type Props = {
+  repo: Repo;
+};
+
+const RepositoryCard = ({ repo }: Props) => {
+  const { description, link, owner, ownerImgSrc, title } = repo;
+  return (
+    <div
+      className="max-w-xs rounded-xl border border-purple-600 bg-gray-900 sm:max-w-prose"
+      key={link}
+    >
+      <div className="flex flex-row gap-4 rounded-lg bg-purple-600 px-4 py-2">
+        <div className="grid place-content-center">
+          <img
+            src={ownerImgSrc}
+            alt={`${owner}'s Avatar`}
+            className="h-14 w-14 rounded-full border border-gray-400"
+            loading="lazy"
+          />
+        </div>
+        <div className="">
+          <h2 className="text-xl font-semibold">{title}</h2>
+          <h3 className="text-lg">by {owner}</h3>
+        </div>
+      </div>
+      <div className="p-4">
+        <p className="overflow-hidden text-ellipsis text-gray-400">
+          {description}
+        </p>
+        <a
+          href={'https://www.github.com' + link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-2 inline-block text-purple-600 hover:underline"
+        >
+          View on GitHub
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default RepositoryCard;
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import Layout from '../../components/Layout';
 import LanguageSelect from './components/LanguageSelect';
 import Loading from '../../components/Loading';
 import { useRepositories } from '../../hooks/useRepositories';
+import RepositoryCard from '../../components/RepositoryCard';
 
 const Home = () => {
   const [language, setLanguage] = useState('javascript');
@@ -28,38 +29,7 @@ const Home = () => {
           {!!repositories ? (
             <div className="no-scrollbar grid flex-1 justify-center gap-5 overflow-auto px-10">
               {repositories.map((repo) => (
-                <div
-                  className="max-w-xs rounded-xl border border-purple-600 bg-gray-900 sm:max-w-prose"
-                  key={repo.link}
-                >
-                  <div className="flex flex-row gap-4 rounded-lg bg-purple-600 px-4 py-2">
-                    <div className="grid place-content-center">
-                      <img
-                        src={repo.ownerImgSrc}
-                        alt={`${repo.owner}'s Avatar`}
-                        className="h-14 w-14 rounded-full border border-gray-400"
-                        loading="lazy"
-                      />
-                    </div>
-                    <div className="">
-                      <h2 className="text-xl font-semibold">{repo.title}</h2>
-                      <h3 className="text-lg">by {repo.owner}</h3>
-                    </div>
-                  </div>
-                  <div className="p-4">
-                    <p className="overflow-hidden text-ellipsis text-gray-400">
-                      {repo.description}
-                    </p>
-                    <a
-                      href={'https://www.github.com' + repo.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="mt-2 inline-block text-purple-600 hover:underline"
-                    >
-                      View on GitHub
-                    </a>
-                  </div>
-                </div>
+                <RepositoryCard repo={repo} />
               ))}
             </div>
           ) : (
