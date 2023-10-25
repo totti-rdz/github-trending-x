858fe6f9bed087bf948a1162a42805db44bdf568
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-25 05:32:00 +0200

    fix Language Array type when fetching

--- client/src/hooks/useLanguageOptions.ts
+++ client/src/hooks/useLanguageOptions.ts
@@ -3,9 +3,9 @@ import { useFetch } from './useFetch';
 export type Language = { label: string; value: string };
 
 export const useLanguageOptions = () => {
-  const [languageOptions, isLoading] = useFetch<Language>(
+  const [languageOptions, isLoading] = useFetch<Language[]>(
     '/api/programming-languages'
-  )
+  );
 
   return { languageOptions, isLoading };
 };
