b52ecdc7f42d2ae2165d655d980843fd4539f394
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-25 05:11:11 +0200

    fix data not resetting when error occurs while fetching

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
 import { ApiService } from '../services/ApiService';
 
-export const useFetch = <T>(url: string, deps: unknown[] = []) => {
-  const [data, setData] = useState<T[] | null>(null);
+export const useFetch = <T>(
+  url: string,
+  deps: unknown[] = [],
+  initialValue: T | null = null
+) => {
+  const [data, setData] = useState<T | null>(initialValue);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const apiService = new ApiService();
@@ -12,6 +16,8 @@ export const useFetch = <T>(url: string, deps: unknown[] = []) => {
         setIsLoading(true);
         const data = await apiService.fetch<T>(url);
         if (!!data) setData(data);
+        // if data is falsy, reset to initial value
+        else setData(initialValue);
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
@@ -29,5 +35,5 @@ export const useFetch = <T>(url: string, deps: unknown[] = []) => {
     };
   }, deps);
 
-  return [data, isLoading] as const;
+  return [data, isLoading, status] as const;
 };
