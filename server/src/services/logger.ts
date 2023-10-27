303dd266154e8795675f3b6c0e0e7ef861b7da5e
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-27 21:01:18 +0200

    enable logger methods to take string arrays

--- server/src/services/logger.ts
+++ server/src/services/logger.ts
@@ -1,21 +1,21 @@
 export const logger = {
-  log: (message: string) => console.log(message),
-  info: (message: string) =>
+  log: (...message: string[]) => console.log(message.join(' ')),
+  info: (...message: string[]) =>
     console.info(
       '\x1b[33m%s\x1b[0m', // change font color to yellow
-      '[INFO] ' + message,
+      '[INFO] ' + message.join(' '),
       '\x1b[0m' // reset font color
     ),
-  warn: (message: string) =>
+  warn: (...message: string[]) =>
     console.error(
       '\x1b[33m%s\x1b[0m', // change font color to yellow
-      '[WARNING] ' + message,
+      '[WARNING] ' + message.join(' '),
       '\x1b[0m' // reset font color
     ),
-  error: (message: string) =>
+  error: (...message: string[]) =>
     console.error(
       '\x1b[31m%s\x1b[0m', // change font color to red
-      '[Error] ' + message,
+      '[Error] ' + message.join(' '),
       '\x1b[0m' // reset font color
     ),
 };
