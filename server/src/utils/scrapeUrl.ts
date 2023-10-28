0a006bab79691ca879c1a1e98aeaf98c7b2439dd
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-28 15:59:57 +0200

    remove console.log

--- server/src/utils/scrapeUrl.ts
+++ server/src/utils/scrapeUrl.ts
@@ -9,7 +9,6 @@ const scrapeUrl = async (url: string) => {
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36',
       },
     });
-    console.log('response', response);
     return response.data;
   } catch (error) {
     httpErrorHandler(error);
