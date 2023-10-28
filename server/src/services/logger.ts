41fb7373b468a3fc5fbba0bcd74bd103ca049b6b
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-28 15:53:42 +0200

    Use type of console.log parameters for logger method

--- server/src/services/logger.ts
+++ server/src/services/logger.ts
@@ -1,18 +1,20 @@
+type Loggable = Parameters<typeof console.log>;
+
 export const logger = {
-  log: (...message: string[]) => console.log(message.join(' ')),
-  info: (...message: string[]) =>
+  log: (...message: Loggable) => console.log(message.join(' ')),
+  info: (...message: Loggable) =>
     console.info(
       '\x1b[33m%s\x1b[0m', // change font color to yellow
       '[INFO] ' + message.join(' '),
       '\x1b[0m' // reset font color
     ),
-  warn: (...message: string[]) =>
+  warn: (...message: Loggable) =>
     console.error(
       '\x1b[33m%s\x1b[0m', // change font color to yellow
       '[WARNING] ' + message.join(' '),
       '\x1b[0m' // reset font color
     ),
-  error: (...message: string[]) =>
+  error: (...message: Loggable) =>
     console.error(
       '\x1b[31m%s\x1b[0m', // change font color to red
       '[Error] ' + message.join(' '),
