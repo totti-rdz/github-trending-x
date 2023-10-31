e0a85969b5872a8ff5efc7684d4bf1ab9aab9fd2
totti-rdz <thorsten_herfurtner@gmx.de>
2023-10-31 04:19:39 +0100

    create useDelepors hook to query trending developers

---
+++ client/src/hooks/useDevelopers.ts
@@ -0,0 +1,26 @@
+import { useFetch } from './useFetch';
+
+export type Developer = {
+  avatarImgSrc: string | undefined;
+  company: string;
+  dateJoined?: string;
+  link: string | undefined;
+  id: number;
+  name: string;
+  popularRepo: {
+    title: string;
+    description?: string;
+    link?: string;
+  };
+  userName: string;
+};
+
+// TODO: add language param to API route for developers; add language param to url here
+export const useDevelopers = (language: string) => {
+  const [developers, isLoading, status] = useFetch<Developer[]>(
+    '/api/developers/',
+    [language]
+  );
+
+  return { developers, isLoading, status };
+};
