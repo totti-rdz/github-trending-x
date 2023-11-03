8ecb83c5e19fcff1ed841e5c6dc543ef7fe9ba09
totti-rdz <thorsten_herfurtner@gmx.de>
2023-11-03 01:53:35 +0100

    extract hover animation classes to function

--- client/src/components/Layout/components/SocialLink.tsx
+++ client/src/components/Layout/components/SocialLink.tsx
@@ -1,29 +1,21 @@
 import SocialIcon from '../../icons/SocialIcon';
 import { StringHelper } from '../../../utils/StringHelper';
-import { classNames } from '../../../utils/classNames';
+import { getHoverAnimationClasses } from '../../../utils/getHoverAnimationClasses';
 
 type Props = { href: string; icon: SocialIcon };
 
 const SocialLink = ({ href, icon }: Props) => {
   const title = StringHelper.capitalize(icon);
+  const hoverAnimationClass = getHoverAnimationClasses({ isIcon: true });
 
   return (
     <a
       href={href}
-      className={classNames(
-        'group block rounded-lg p-1',
-        'hover:scale-105 hover:bg-gray-700 hover:text-white'
-      )}
+      className={hoverAnimationClass.parent}
       target="_blank"
       title={title}
     >
-      <SocialIcon
-        icon={icon}
-        className={classNames(
-          'h-5 w-5 transition-transform',
-          'group-hover:rotate-12 group-hover:scale-110'
-        )}
-      />
+      <SocialIcon icon={icon} className={hoverAnimationClass.child} />
     </a>
   );
 };
---
+++ client/src/utils/getHoverAnimationClasses.ts
@@ -0,0 +1,26 @@
+import { classNames } from './classNames';
+
+const duration = 'duration-150';
+const timingFunction = 'ease-out';
+
+type Options = {
+  isIcon?: boolean;
+};
+
+export const getHoverAnimationClasses = ({ isIcon }: Options = {}) => {
+  const parent = classNames(
+    `group inline-block rounded-lg transition ${duration} ${timingFunction}`,
+    isIcon ? 'p-1' : 'px-2 py-1',
+    'hover:scale-105 hover:bg-gray-700 hover:text-white'
+  );
+  const child = classNames(
+    `inline-block transition ${duration} ${timingFunction}`,
+    `group-hover:scale-110`,
+    isIcon ? 'group-hover:rotate-12' : 'group-hover:rotate-3'
+  );
+
+  return {
+    parent,
+    child,
+  };
+};
