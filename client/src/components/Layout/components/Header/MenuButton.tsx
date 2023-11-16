f3223c39ee7dccca46ad1d839033dfcb24d64538
totti-rdz <thorsten_herfurtner@gmx.de>
2023-11-16 23:10:46 +0100

    add aria-controls to MenuButton

--- client/src/components/Layout/components/Header/MenuButton.tsx
+++ client/src/components/Layout/components/Header/MenuButton.tsx
@@ -3,16 +3,18 @@ import MenuIcon from '../../../icons/MenuIconAnimated';
 type Props = {
   className?: string;
   isOpen: boolean;
+  menuId: string;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const MenuButton = ({ className, isOpen, setIsOpen }: Props) => {
+const MenuButton = ({ className, isOpen, menuId, setIsOpen }: Props) => {
   return (
     <div className={className}>
       <button
         className="group h-10 w-10 overflow-hidden rounded-full border-2 border-purple-600 p-1"
         onClick={() => setIsOpen((current) => !current)}
         aria-label={`${isOpen ? 'Close' : 'Open'} menu`}
+        aria-controls={menuId}
         aria-expanded={`${isOpen}`}
       >
         <div
--- client/src/components/Layout/components/Header/MenuMobile.tsx
+++ client/src/components/Layout/components/Header/MenuMobile.tsx
@@ -6,14 +6,14 @@ type Link = {
   href: string;
 };
 
-type Props = { isOpen: boolean; links: Link[] };
+type Props = { id: string; isOpen: boolean; links: Link[] };
 
-const MenuMobile = ({ isOpen, links }: Props) => {
+const MenuMobile = ({ id, isOpen, links }: Props) => {
   if (!isOpen) return <></>;
 
   return (
     <div className="absolute left-0 top-0 z-40 h-full w-full bg-background pb-24 pt-12 duration-500">
-      <nav className="my-10" aria-label="primary">
+      <nav className="my-10" aria-label="primary" id={id}>
         <ul className="flex flex-col items-center">
           {links.map(({ label, href }) => (
             <NavLinkMobile href={href} key={label}>
