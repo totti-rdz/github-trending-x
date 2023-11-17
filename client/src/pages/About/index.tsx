a1b55dcd6b1837da1edf5c388a2a21f6316ee13c
totti-rdz <thorsten_herfurtner@gmx.de>
2023-11-17 04:03:48 +0100

    create About page

---
+++ client/src/pages/About/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '../../components/Layout';
+import Title from '../../components/Title';
+
+const About = () => {
+  return (
+    <Layout>
+      <div className="grid justify-center">
+        <Title text="About" />
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
