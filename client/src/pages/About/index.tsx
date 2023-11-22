884bea3aa3c724c96ec3726a07f0bf84181c8467
totti-rdz <thorsten_herfurtner@gmx.de>
2023-11-22 09:52:48 +0100

    add static about text to About page

--- client/src/pages/About/index.tsx
+++ client/src/pages/About/index.tsx
@@ -6,6 +6,16 @@ const About = () => {
     <Layout>
       <div className="grid justify-center">
         <Title text="About" />
+        <div className="my-5 w-[65ch]">
+          <h2 className="mb-4 text-2xl font-bold text-accent-600">
+            This project:
+          </h2>
+          <p>
+            A monorepo featuring an Express backend and a single-page React
+            application powered by Vite, all in TypeScript - with the main focus
+            on learning, e.g. web scraping with cheerio.
+          </p>
+        </div>
       </div>
     </Layout>
   );
