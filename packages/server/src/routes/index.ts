6d8c0396dd5f685ac0517b3e642eb61d27707da9
Totti Rdz <thorsten_herfurtner@gmx.de>
2024-11-16 04:13:10 +0100

    feat: add health check endpoint to API routes

--- packages/server/src/routes/index.ts
+++ packages/server/src/routes/index.ts
@@ -6,7 +6,12 @@ const router = express.Router();
 
 export const routes = () => {
   // add api routes here in correct order
+
   router.use(trendingReposRouter);
+
+  router.get('/health', (_, res) => {
+    res.status(200).send("ok");
+  });
 
   router.get('*', (_, res) => {
     res.status(404).send();
