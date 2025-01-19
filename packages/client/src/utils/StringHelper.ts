668479e52a4231194d3cd356d57b12db27c66617
Totti Rdz <thorsten_herfurtner@gmx.de>
2025-01-19 01:50:50 +0100

    fix: redundant type conversion in classNames function
    by updating StringHelper.isValidString to use a type predicate to improve type safety

--- packages/client/src/utils/StringHelper.ts
+++ packages/client/src/utils/StringHelper.ts
@@ -2,6 +2,6 @@ export const StringHelper = {
   capitalize: (string: string) =>
     string.charAt(0).toUpperCase() + string.slice(1),
 
-  isValidString: (string: unknown) =>
+  isValidString: (string: unknown): string is string =>
     typeof string === 'string' && string.trim() !== '',
 };
